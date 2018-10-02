efd6f72a6edbc4c1873d2d0d5f888816d13c054e
Juan M. Garcia <garcia.solutions@gmail.com>
2018-10-02 15:39:07 -0500

    initial commit

--- src/store.js
+++ src/store.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import feathersVuex from 'feathers-vuex'
+import feathersClient from './feathers-client'
+const { FeathersVuex, service } = feathersVuex(feathersClient, { idField: '_id' })
 Vue.use(Vuex)
+Vue.use(FeathersVuex)
 
 export default new Vuex.Store({
+  plugins: [
+    service('clients', {
+      instanceDefaults: {
+        text: ''
+      }
+    })
+  ],
   state: {
 
   },
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input @keyup.enter="newClient" v-model="text" type="text">
+    <button @click="newClient">
+      Crear Nuevo Cliente
+    </button>
+    <div v-for="client in clients" :key="client._id">
+      {{client.text}}
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'home',
+  data () {
+    return {
+      text: '',
+      clients: []
+    }
+  },
+  methods: {
+    ...mapActions('clients', { getClientsFromService: 'find' }),
+    newClient () {
+      const { Client } = this.$FeathersVuex
+      let cliente = new Client({ text: this.text })
+      cliente.save().then((res) => {
+        this.getClientsFromService().then(res => {
+          this.text = ''
+          this.clients = res.data
+        })
+      })
+    }
+  },
+  computed: {
+    ...mapGetters('clients', { getClientsInStore: 'find' }),
+    getClients () {
+      return this.getClientsInStore().data
+    }
+  },
+
+  created () {
+    console.log('este es el getter', this.getClients)
+    this.getClientsFromService().then(res => {
+      this.clients = res.data
+    })
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
